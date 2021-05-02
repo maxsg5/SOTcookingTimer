4703c7ff6c5fc45ca5f60ccbb1cdf3ef50446fd3
Max Schafer <maxsg5@gmail.com>
2021-05-01 18:19:20 -0600

    u

--- newCookingTimer/index.js
+++ newCookingTimer/index.js
@@ -4,16 +4,20 @@ var cancelTimer = false;
 
 function startTimer(foodTypeTime) {
   if(!activeTimer){
+      cancelTimer = false;
       activeTimer = true;
       document.getElementById("body").style.animationPlayState = "paused";
       document.getElementById("body").style.backgroundColor = "white";
 
 
-      //var timeleft = 5;
       var downloadTimer = setInterval(function(){
         if(cancelTimer){
+          document.getElementById("countdown").innerHTML = "Canceled!";
           clearInterval(downloadTimer);
           cancelTimer = false;
+          activeTimer = false;
+          return;
+          
         }
         if(foodTypeTime <= 0){
           clearInterval(downloadTimer);
@@ -23,119 +27,13 @@ function startTimer(foodTypeTime) {
           //document.getElementById("body").style.animationPlayState = "running";
           activeTimer = false;
         } else {
-          document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+          document.getElementById("countdown").innerHTML = foodTypeTime + " seconds remaining";
         }
-        timeleft -= 1;
+        foodTypeTime -= 1;
       }, 1000);
   }
 }
-
-function startFishTimer() {
-    if(!activeTimer){
-        activeTimer = true;
-        document.getElementById("body").style.animationPlayState = "paused";
-        document.getElementById("body").style.backgroundColor = "white";
-
-
-        var timeleft = 5;
-        var downloadTimer = setInterval(function(){
-          if(timeleft <= 0){
-            clearInterval(downloadTimer);
-            document.getElementById("countdown").innerHTML = "Finished!";
-            var audio = new Audio('./sound/sizzle.mp3');
-            audio.play();
-            //document.getElementById("body").style.animationPlayState = "running";
-            activeTimer = false;
-          } else {
-            document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-          }
-          timeleft -= 1;
-        }, 1000);
-    }
-}
-
-function startTrophyFishTimer() {
-    if(!activeTimer){
-        activeTimer = true;
-        document.getElementById("body").style.animationPlayState = "paused";
-        document.getElementById("body").style.backgroundColor = "white";
-
-        var timeleft = 90;
-        var downloadTimer = setInterval(function(){
-          if(timeleft <= 0){
-            clearInterval(downloadTimer);
-            document.getElementById("countdown").innerHTML = "Finished!";
-            var audio = new Audio('./sound/sizzle.mp3');
-            audio.play();
-            activeTimer = false;
-          } else {
-            document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-          }
-          timeleft -= 1;
-        }, 1000);
-    }
-}
-
-function startMeatTimer() {
-    if(!activeTimer){
-        activeTimer = true;
-        document.getElementById("body").style.animationPlayState = "paused";
-
-        var timeleft = 60;
-        var downloadTimer = setInterval(function(){
-          if(timeleft <= 0){
-            clearInterval(downloadTimer);
-            document.getElementById("countdown").innerHTML = "Finished!";
-            var audio = new Audio('./sound/sizzle.mp3');
-            audio.play();
-            activeTimer = false;
-          } else {
-            document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-          }
-          timeleft -= 1;
-        }, 1000);
-    }
-}
-
-
-function startMegladonMeatTimer() {
-    if(!activeTimer){
-        activeTimer = true;
-        document.getElementById("body").style.animationPlayState = "paused";
-
-        var timeleft = 120;
-        var downloadTimer = setInterval(function(){
-          if(timeleft <= 0){
-            clearInterval(downloadTimer);
-            document.getElementById("countdown").innerHTML = "Finished!";
-            var audio = new Audio('./sound/sizzle.mp3');
-            audio.play();
-            activeTimer = false;
-          } else {
-            document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-          }
-          timeleft -= 1;
-        }, 1000);
-    }
-}
-
-function startKrakenMeatTimer() {
-    if(!activeTimer){
-        activeTimer = true;
-        document.getElementById("body").style.animationPlayState = "paused";
-
-        var timeleft = 120;
-        var downloadTimer = setInterval(function(){
-          if(timeleft <= 0){
-            clearInterval(downloadTimer);
-            document.getElementById("countdown").innerHTML = "Finished!";
-            var audio = new Audio('./sound/sizzle.mp3');
-            audio.play();
-            activeTimer = false;
-          } else {
-            document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-          }
-          timeleft -= 1;
-        }, 1000);
-    }
+function CancelTimer(){
+  cancelTimer = true;
+  
 }
